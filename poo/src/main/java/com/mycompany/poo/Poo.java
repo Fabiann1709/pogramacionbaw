647001383aaef1d8c4130d0d59835b68a7b7d732
Fabiann1709 <fabialejo.1709@gmail.com>
2024-02-29 15:21:08 -0500

    a la grande le puse cuca

--- poo/src/main/java/com/mycompany/poo/Poo.java
+++ poo/src/main/java/com/mycompany/poo/Poo.java
@@ -1,16 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.poo;
 
-/**
- *
- * @author SCIS2PC09
- */
+
 public class Poo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Character homero = new Character("Homero", 39, 'M', "A LA GRANDE LE PUSE CUCA" );
+        homero.saySomething();
+        Character bart = new Character("bart", 12, 'M', "Ay caramba" );
+        bart.saySomething();
+    }
+    
+    static class Character{
+        String name;
+        int age;
+        char gender;
+        String phrase;
+
+        public Character(String name, int age, char gender, String phrase) {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.phrase = phrase;
+        }
+        
+        void saySomething(){
+            System.out.println(this.phrase);
+        }
     }
 }
